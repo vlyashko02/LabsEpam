aa3499990f396de50993c6f320d707a0af216aea
Vladimir Lyashko <vlyashko.work@gmail.com>
2021-01-09 19:00:16 +0300

    Четвертая лаба

---
+++ lab4/lab4.BL/TimeDisplay.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace lab4.BL
+{
+    public class TimeDisplay
+    {
+        int seconds;
+        int minutes;
+        int hours;
+
+        public int Seconds
+        {
+            get
+            {
+                return seconds;
+            }
+            set
+            {
+                if(value >= 0 && value <= 60)
+                {
+                    if (value == 60)
+                    {
+                        seconds = 0;
+                        minutes++;
+                        minutes = ValidateMinutesAndSeconds(minutes, out int period);
+                        if (period > 0)
+                        {
+                            hours += period;
+                            hours = ValidateHours(hours);
+                        }
+                    }
+                    else
+                        seconds = value;
+                }
+                else
+                    throw new ArgumentException("Seconds should be from 0 to 60.");
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+            set
+            {
+                if(value >= 0 && value <= 60)
+                {
+                    if(value == 60)
+                    {
+                        minutes = 0;
+                        hours++;
+                        hours = ValidateHours(hours);
+                    }
+                    else
+                        minutes = value;
+                }
+                else
+                    throw new ArgumentException("Minutes should be from 0 to 60.");
+            }
+        }
+        public int Hours
+        {
+            get
+            {
+                return hours;
+            }
+            set
+            {
+                if (value >= 0 && value <= 24)
+                {
+                    if (value == 24)
+                        hours = 0;
+                    else
+                        hours = value;
+                }
+                else
+                    throw new ArgumentException("Hours should be from 0 to 24.");
+            }
+        }
+
+        public TimeDisplay()
+        {
+
+        }
+        public TimeDisplay(int seconds, int minutes, int hours)
+        {
+            seconds = ValidateMinutesAndSeconds(seconds, out int period);
+            if (period > 0)
+                minutes += period;
+            this.seconds = seconds;
+            minutes = ValidateMinutesAndSeconds(minutes, out period);
+            if (period > 0)
+                hours += period;
+            this.minutes = minutes;
+            hours = ValidateHours(hours);
+            this.hours = hours;
+        }
+        public void ChangeHours(int hours)
+        {
+            this.hours += hours;
+            this.hours = ValidateHours(this.hours);
+        }
+        public void ChangeMinutes(int minutes)
+        {
+            this.minutes += minutes;
+            int period = this.minutes / 60;
+            if(period > 0)
+            {
+                this.minutes -= 60 * period;
+                hours += period;
+                hours = ValidateHours(hours);
+            }
+        }
+        public void ChangeSeconds(int seconds)
+        {
+            this.seconds += seconds;
+            int period = this.seconds / 60;
+            if(period > 0)
+            {
+                this.seconds -= 60 * period;
+                minutes += period;
+                int periodM = minutes / 60;
+                if(periodM > 0)
+                {
+                    minutes -= 60 * period;
+                    hours += periodM;
+                    hours = ValidateHours(hours);
+                }
+            }
+        }
+        private int ValidateHours(int hours)
+        {
+            int period = hours / 24;
+            if (period > 0)
+                hours -= 24 * period;
+            return hours;
+        }
+        private int ValidateMinutesAndSeconds(int time, out int period)
+        {
+            period = time / 60;
+            if (period > 0)
+                time -= 60 * period;
+            return time;
+        }
+    }
+}
---
+++ lab4/lab4.CMD/Program.cs
@@ -0,0 +1,175 @@
+﻿using lab4.BL;
+using System;
+
+namespace lab4.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4 Menu");
+            Console.WriteLine("Press 1 to create constructor without parameters");
+            Console.WriteLine("Press 2 to create constructor with parameters");
+            if (!int.TryParse(Console.ReadLine(), out int k))
+                throw new Exception("You should write 1 or 2.");
+            switch(k)
+            {
+                case 1:
+                    FirstKeyMenu();
+                    break;
+                case 2:
+                    SecondKeyMenu();
+                    break;
+                default:
+                    throw new ArgumentException("You should write 1 or 2.");
+            }
+        }
+        private static void FirstKeyMenu()
+        {
+            TimeDisplay timeDisplay = new TimeDisplay();
+            MainMenu(timeDisplay);
+        }
+        private static void SecondKeyMenu()
+        {
+            Console.Write("Enter hours: ");
+            if (int.TryParse(Console.ReadLine(), out int hours))
+            {
+                if (hours >= 0 && hours <= 24)
+                {
+                    Console.Write("Enter minutes: ");
+                    if (int.TryParse(Console.ReadLine(), out int minutes))
+                    {
+                        if (minutes >= 0 && minutes <= 60)
+                        {
+                            Console.Write("Enter seconds: ");
+                            if (int.TryParse(Console.ReadLine(), out int seconds))
+                            {
+                                if (seconds >= 0 && seconds <= 60)
+                                {
+                                    TimeDisplay timeDisplay = new TimeDisplay(seconds, minutes, hours);
+                                    MainMenu(timeDisplay);
+                                }
+                                else
+                                    throw new ArgumentException("Seconds should be between 0 and 60.");
+                            }
+                            else
+                                throw new ArgumentException("Seconds should be an integer.");
+                        }
+                        else
+                            throw new ArgumentException("Minutes should be between 0 and 60.");
+                    }
+                    else
+                        throw new ArgumentException("Minutes should be an integer.");
+                }
+                else
+                    throw new ArgumentException("Hours should be between 0 and 24.");
+            }
+            else
+                throw new ArgumentException("Hours should be an integer.");
+        }
+        private static void MainMenu(TimeDisplay timeDisplay)
+        {
+            Console.Clear();
+            FormatViewTime(timeDisplay);
+            Console.WriteLine();
+            MenuView();
+            string input = Console.ReadLine();
+
+            while (input != "exit")
+            {
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Set hours: ");
+                        if (int.TryParse(Console.ReadLine(), out int hours))
+                        {
+                            timeDisplay.Hours = hours;
+                        }
+                        else
+                            throw new ArgumentException("Hours should be an integer.");
+                        break;
+                    case "2":
+                        Console.Write("Set minutes: ");
+                        if (int.TryParse(Console.ReadLine(), out int minutes))
+                        {
+                            timeDisplay.Minutes = minutes;
+                        }
+                        else
+                            throw new ArgumentException("Minutes should be an integer.");
+                        break;
+                    case "3":
+                        Console.Write("Set seconds: ");
+                        if (int.TryParse(Console.ReadLine(), out int seconds))
+                        {
+                            timeDisplay.Seconds = seconds;
+                        }
+                        else
+                            throw new ArgumentException("Seconds should be an integer.");
+                        break;
+                    case "4":
+                        Console.Write("Number of hours to add: ");
+                        if (int.TryParse(Console.ReadLine(), out int hoursAdd))
+                        {
+                            if (hoursAdd > 0)
+                                timeDisplay.ChangeHours(hoursAdd);
+                        }
+                        else
+                            throw new ArgumentException("Hours should be an integer.");
+                        break;
+                    case "5":
+                        Console.Write("Number of minutes to add: ");
+                        if (int.TryParse(Console.ReadLine(), out int minutesAdd))
+                        {
+                            if (minutesAdd > 0)
+                                timeDisplay.ChangeMinutes(minutesAdd);
+                        }
+                        else
+                            throw new ArgumentException("Minutes should be an integer.");
+                        break;
+                    case "6":
+                        Console.Write("Number of seconds to add: ");
+                        if (int.TryParse(Console.ReadLine(), out int secondsAdd))
+                        {
+                            if (secondsAdd > 0)
+                                timeDisplay.ChangeSeconds(secondsAdd);
+                        }
+                        else
+                            throw new ArgumentException("Seconds should be an integer.");
+                        break;
+                }
+                Console.Clear();
+                FormatViewTime(timeDisplay);
+                Console.WriteLine();
+                MenuView();
+                input = Console.ReadLine();
+            }
+        }
+        private static void MenuView()
+        {
+            Console.WriteLine("1 - Set hours");
+            Console.WriteLine("2 - Set minutes");
+            Console.WriteLine("3 - Set seconds");
+            Console.WriteLine("4 - Change hours");
+            Console.WriteLine("5 - Change minutes");
+            Console.WriteLine("6 - Change seconds");
+            Console.WriteLine("exit - Close app");
+        }
+        private static void FormatViewTime(TimeDisplay timeDisplay)
+        {
+            if(timeDisplay.Hours < 10 && timeDisplay.Minutes < 10 && timeDisplay.Seconds < 10)
+                Console.WriteLine($"Current time: 0{timeDisplay.Hours}:0{timeDisplay.Minutes}:0{timeDisplay.Seconds}");
+            if (timeDisplay.Hours < 10 && timeDisplay.Minutes < 10 && timeDisplay.Seconds >= 10)
+                Console.WriteLine($"Current time: 0{timeDisplay.Hours}:0{timeDisplay.Minutes}:{timeDisplay.Seconds}");
+            if (timeDisplay.Hours < 10 && timeDisplay.Minutes >= 10 && timeDisplay.Seconds >= 10)
+                Console.WriteLine($"Current time: 0{timeDisplay.Hours}:{timeDisplay.Minutes}:{timeDisplay.Seconds}");
+            if (timeDisplay.Hours >= 10 && timeDisplay.Minutes >= 10 && timeDisplay.Seconds >= 10)
+                Console.WriteLine($"Current time: {timeDisplay.Hours}:{timeDisplay.Minutes}:{timeDisplay.Seconds}");
+            if (timeDisplay.Hours >= 10 && timeDisplay.Minutes >= 10 && timeDisplay.Seconds < 10)
+                Console.WriteLine($"Current time: {timeDisplay.Hours}:{timeDisplay.Minutes}:0{timeDisplay.Seconds}");
+            if (timeDisplay.Hours >= 10 && timeDisplay.Minutes < 10 && timeDisplay.Seconds < 10)
+                Console.WriteLine($"Current time: {timeDisplay.Hours}:0{timeDisplay.Minutes}:0{timeDisplay.Seconds}");
+            if (timeDisplay.Hours >= 10 && timeDisplay.Minutes < 10 && timeDisplay.Seconds >= 10)
+                Console.WriteLine($"Current time: {timeDisplay.Hours}:0{timeDisplay.Minutes}:{timeDisplay.Seconds}");
+        }
+    }
+}
